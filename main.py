551c87c0cf20d3e0b8077b5c7cf3e3ea1210f94c
Fraser Samson <2661506S@student.gla.ac.uk>
2024-11-16 17:15:45 +0000

    Replaced A-star with breadth first search, added teleportation links, fixed get neighbours function

--- main.py
+++ main.py
@@ -1,58 +1,59 @@
-from queue import PriorityQueue
+from collections import deque
 
 from mazelib import Maze
 from mazelib.generate.Prims import Prims
 import random
 
+
 def insert_teleportation_links_into_maze(maze, t):
     # Solve the maze to first get a solution for the shortest path
-    solution = astar_solve(maze)
+    shortest_path = astar_solve(maze, dict())
 
-    # Do not insert teleportation links along the shortest path, to ensure the maze will still always be solveable
-    cells_not_to_insert_solutions = solution.keys()
+    # Do not insert teleportation links along the shortest path, to ensure the maze will still always be solvable
+    cells_not_to_insert_links = shortest_path
 
     all_empty_cells = []
     for rowIndex, row in enumerate(maze.grid):
         for columnIndex, cell in enumerate(row):
             # If empty and not
-            if cell == 0 and (rowIndex, columnIndex) not in cells_not_to_insert_solutions:
+            if cell == 0 and (rowIndex, columnIndex) not in cells_not_to_insert_links:
                 all_empty_cells.append((rowIndex, columnIndex))
 
     # Sample empty cells to get entrance and exits of teleportation links
-    cells_to_add_teleportation_links = random.sample(all_empty_cells, t*2)
+    cells_to_add_teleportation_links = random.sample(all_empty_cells, t * 2)
 
     # Create teleportation links between sampled empty cells.
     teleportation_links = dict()
-    for i in range(0, t, 2):
-        entrance_cell = cells_to_add_teleportation_links[t]
-        exit_cell = cells_to_add_teleportation_links[t+1]
+    for i in range(0, t * 2, 2):
+        entrance_cell = cells_to_add_teleportation_links[i]
+        exit_cell = cells_to_add_teleportation_links[i + 1]
 
         teleportation_links[entrance_cell] = exit_cell
 
     return teleportation_links
 
+
 def get_neighbours(maze, current_row, current_col, teleportation_links):
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     height, width = len(maze.grid), len(maze.grid[0])
 
-    possible_neighbours = [
-        (current_row+rowOffset, current_col+columnOffset) for rowOffset, columnOffset in directions
+    neighbours = [
+        (current_row + rowOffset, current_col + columnOffset) for rowOffset, columnOffset in directions
     ]
+    if [cell for cell in neighbours if cell in teleportation_links]:
+        pass
 
     # Remove out of bounds
-    neighbours_in_bounds = [(row, col) for (row, col) in possible_neighbours if 0 <= row < height and 0 <= col < width]
+    neighbours = [(row, col) for (row, col) in neighbours if 0 <= row < height and 0 <= col < width]
 
     # Remove neighbours which are walls and are not entrance/exits
-    neighbours_without_walls = [
-        (row, col) for (row, col) in neighbours_in_bounds if maze.grid[row][col] == 0 or (row, col) in (maze.start, maze.end)]
+    neighbours = [
+        (row, col) for (row, col) in neighbours if maze.grid[row][col] == 0 or (row, col) in (maze.start, maze.end)]
 
     # If neighbouring cell is a teleportation link, replace the neighbour with the exit cell of the link
-    neighbours_with_teleport_replacements = [
-        teleportation_links.get(neighbour, neighbour) for neighbour in neighbours_without_walls
-    ]
+    neighbours = [teleportation_links.get(neighbour, neighbour) for neighbour in neighbours]
+    return neighbours
 
-    print(f'Neighbours of ({current_row}, {current_col}): {neighbours_with_teleport_replacements}')
-    return neighbours_with_teleport_replacements
 
 def get_maze_with_teleportation_links(n, t):
     m = Maze()
@@ -60,44 +61,32 @@ def get_maze_with_teleportation_links(n, t):
     m.generate()
     m.generate_entrances(True, True)
 
-    teleportation_links = dict()
+    teleportation_links = insert_teleportation_links_into_maze(m, t)
 
     return m, teleportation_links
 
-def h(neighbour, end):
-    return 0
 
 def astar_solve(maze, teleportation_links):
-    print(maze)
-    frontier = PriorityQueue()
+    frontier = deque()
     start_position = maze.start
     parent_nodes = dict()
-    distance = dict()
 
     parent_nodes[start_position] = None
-    distance[start_position] = 0
-    frontier.put((0, start_position))
+    frontier.append(start_position)
 
     while frontier:
-        current = frontier.get()[1]
+        current = frontier.popleft()
 
         if current == maze.end:
-            return parent_nodes
+            return reconstruct_solution(parent_nodes, maze.end)
 
-        for neighbour in get_neighbours(maze, current[0], current[1], teleportation_links ):
+        for neighbour in get_neighbours(maze, current[0], current[1], teleportation_links):
             if neighbour not in parent_nodes.keys():
                 parent_nodes[neighbour] = current
-                distance[neighbour] = distance[current] + 1
-
-                f_value =  distance[neighbour] + h(neighbour, maze.end)
-                frontier.put((f_value, neighbour))
-
-    print(maze.grid[start_position[0]][start_position[1]])
-    frontier.put((0, start_position))
-
-
+                frontier.append(neighbour)
 
     return None
+
 
 def reconstruct_solution(previous_nodes, end_node):
     solution_path = []
@@ -110,10 +99,13 @@ def reconstruct_solution(previous_nodes, end_node):
     solution_path.reverse()
     return solution_path
 
-m, teleportation_links = get_maze_with_teleportation_links(10, 2)
-solution = astar_solve(m, teleportation_links)
-if solution is None:
-    raise "No solution was found for maze"
 
-solution_path = reconstruct_solution(solution, m.end)
-print(solution_path)
+if __name__ == '__main__':
+    m, teleportation_links = get_maze_with_teleportation_links(30, 10)
+    print(m)
+    print("teleportation links", teleportation_links)
+    solution = astar_solve(m, teleportation_links)
+    if solution is None:
+        raise "No solution was found for maze"
+
+    print('\n'.join([str(cell) for cell in solution]))
